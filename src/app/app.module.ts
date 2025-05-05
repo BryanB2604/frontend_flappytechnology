feeedd4fa81ae3ef7ea8b0597ab3fe01a247809a
Bryan Betancourt <bbetancourt2604@gmail.com>
2025-05-04 21:24:16 -0500

    Traer cambios para neflity

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; 
-import { provideHttpClient } from '@angular/common/http';  
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TreeTableModule } from 'primeng/treetable';
-import { ReactiveFormsModule } from '@angular/forms';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -33,10 +33,11 @@ import { SocketComponent } from './core/socket/socket.component';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    TreeTableModule
+    TreeTableModule,
+    CommonModule
   ],
   providers: [
-    provideHttpClient()
+    provideHttpClient(withInterceptorsFromDi()) // ✅ Recomendado en Angular 17+
   ],
   bootstrap: [AppComponent]
 })
